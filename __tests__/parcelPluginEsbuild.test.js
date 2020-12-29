b26f59f8f3a81e9af7138ef5dfa973be1492ac49
bung87 <crc32@qq.com>
2020-12-30 01:53:35 +0800

    o

--- __tests__/parcelPluginEsbuild.test.js
+++ __tests__/parcelPluginEsbuild.test.js
@@ -1,19 +1,42 @@
-// Const assert = require("assert");
 const parcelPluginEsbuild = require("../lib/index.js");
-
+const { performance } = require("perf_hooks");
 const Bundler = require("parcel-bundler");
 const path = require("path");
 const assertBundle = require("parcel-assert-bundle-tree");
-// Const fs = require("fs");
 
-// Const readFile = location =>
-//   new Promise((resolve, reject) =>
-//     fs.readFile(location, (err, data) => (err ? reject(err) : resolve(data)))
-//   );
-
-describe("Asset", () => {
+describe("compile ts with builtin typescript", () => {
   it("should ouput the js code...", async () => {
     // Init bundler
+    const t1 = performance.now();
+
+    const bundler = new Bundler(path.join(__dirname, "./example.ts"), {
+      outDir: path.join(__dirname, "build"),
+      watch: false,
+      cache: false,
+      hmr: false,
+      logLevel: 0
+    });
+
+    // Bundle everything
+    const bundle = await bundler.bundle();
+    console.log(`typescript takes ${performance.now() - t1}ms`);
+    assertBundle(bundle, {
+      type: "js",
+      name: "example.js",
+      childBundler: [
+        {
+          type: "map"
+        }
+      ]
+    });
+  }, 25000);
+});
+
+describe("compile ts with esbuild", () => {
+  it("should ouput the js code...", async () => {
+    // Init bundler
+    const t1 = performance.now();
+
     const bundler = new Bundler(path.join(__dirname, "./example.ts"), {
       outDir: path.join(__dirname, "build"),
       watch: false,
@@ -26,6 +49,7 @@ describe("Asset", () => {
 
     // Bundle everything
     const bundle = await bundler.bundle();
+    console.log(`esbuild takes ${performance.now() - t1}ms`);
     assertBundle(bundle, {
       type: "js",
       name: "example.js",
